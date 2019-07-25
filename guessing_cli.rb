02496b7f1cb7137c5a06fb5bb383be0f738dac42
Andrew Rosario <rosario.andrew@gmail.com>
2019-07-25 21:06:45 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,33 @@
 # Code your solution here!
+
+def generate_random_number
+  return rand(5) + 1
+end
+
+def prompt_user
+  puts "Guess a number between 1 and 6:"
+  gets.chomp
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def run_guessing_game
+
+end
